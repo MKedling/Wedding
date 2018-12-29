2bcf0ea1fb9e7d27fdf0c9498cfdb92ef6baef07
MKEDLING <mattias.kedling@axfood.se>
2018-12-29 20:52:33 +0100

    Head

--- app/src/components/Toolbar/Toolbar.js
+++ app/src/components/Toolbar/Toolbar.js
@@ -9,15 +9,19 @@ const toolbar = props => {
   return (
     <header className="toolbar col-xs-12 col-sm-12">
       <nav className="toolbar__navigation">
+
           <div className="toolbar__toggle-button d-flex d-md-none">
             <DrawerToggleButton click={props.drawerClickHandler}/>
           </div>
-          <div className="toolbar__title d-flex col-sm-12 col-md-6"><a href="/" className="text-center">M/S Kedling</a></div>
-          <div className="toolbar_navigation-items d-none d-md-flex col-md-6 float-right">
-            <ul className="float-right">
+
+          <div className="toolbar__title d-flex col-sm-10 col-md-4"><a href="/" className="">M/S Kedling</a></div>
+
+          <div className="toolbar_navigation-items d-none d-md-block col-md-8">
+            <ul className="flexcontainer row">
               {props.links}
             </ul>
           </div>
+
       </nav>
     </header>
   );
